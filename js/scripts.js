15659f698512f86c3cc38cd275c5e3d45948e521
Meliah MacKay <xmeilahmackay@gmail.com>
2020-02-16 13:15:17 -0800

    add initial pages, setup, add name feild with alert function calling back name

---
+++ js/scripts.js
@@ -0,0 +1,9 @@
+var name = $("#name").val();
+
+
+$(document).ready(function() {
+  $("form#apt").submit(function(event) { 
+    var name = $("#name").val();
+    alert((name) + " you have booked your first appointment!");
+  });
+});
